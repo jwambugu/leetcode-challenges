43b79ad7a355d9634f1fbd554434dd86908806de
Joram Wambugu <artisan254@gmail.com>
2021-10-20 12:01:28 +0300

    Attempt two sum challenge

--- golang/main.go
+++ golang/main.go
@@ -9,8 +9,12 @@ func main() {
 	//fmt.Println(findJudge(1, [][]int{})) // 1
 	//fmt.Println(findJudge(2, [][]int{})) // 2
 
-	fmt.Println(smallestPositiveInteger([]int{1, 3, 6, 4, 1, 2})) // 5
-	fmt.Println(smallestPositiveInteger([]int{1, 2, 3}))          // 4
-	fmt.Println(smallestPositiveInteger([]int{-1, -3}))           // 1
-	fmt.Println(smallestPositiveInteger([]int{1, 2, 0}))          // 1
+	//fmt.Println(smallestPositiveInteger([]int{1, 3, 6, 4, 1, 2})) // 5
+	//fmt.Println(smallestPositiveInteger([]int{1, 2, 3}))          // 4
+	//fmt.Println(smallestPositiveInteger([]int{-1, -3}))           // 1
+	//fmt.Println(smallestPositiveInteger([]int{1, 2, 0}))          // 1
+
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // [0,1]
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))      // [1,2]
+	fmt.Println(twoSum([]int{3, 3}, 6))         // [0,1]
 }
---
+++ golang/two-sum.go
@@ -0,0 +1,41 @@
+package main
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// You can return the answer in any order.
+
+// Example 1:
+//
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+//
+//
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+//Example 2:
+//
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+//
+//Example 3:
+//
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+
+func twoSum(nums []int, target int) []int {
+	count := len(nums)
+	memo := make(map[int]int)
+
+	for i := 0; i <= count; i++ {
+		remainder := target - nums[i]
+
+		if _, ok := memo[remainder]; ok {
+
+			return []int{memo[remainder], i}
+		}
+
+		memo[nums[i]] = i
+	}
+
+	return nil
+}
